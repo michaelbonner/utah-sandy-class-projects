5c99b40a0cfb4648e06dd3bd092e2dff0f40ad31
Michael Bonner <michaelwbonner@gmail.com>
2019-10-11 15:06:33 -0600

    order projects alphabetically

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,19 +1,11 @@
 const projects = [
   {
-    title: "What's in Your Pantry",
-    description: "Find a recipe based on the items in your pantry",
-    image: "whats-in-your-pantry.png",
-    url: "https://collinchiz.github.io/Project-1/",
-    github: "https://github.com/collinchiz/Project-1",
-    youtube: "https://youtu.be/n5rXBLownQs",
-  },
-  {
-    title: "Stock Stop",
-    description: "Simplified listing of stock information with news",
-    image: "stock-stop.png",
-    url: "https://kspaffk.github.io/Project1/",
-    github: "https://github.com/kspaffk/Project1",
-    youtube: "https://youtu.be/vIWwl1OMZkw",
+    title: "Simplicity Via Clarity",
+    description: "See who is on your website and what they are doing",
+    image: "simplicity-via-clarity.png",
+    url: "https://simplicityviaclarity.com/",
+    github: "https://github.com/ccollins1544/simplicityviaclarity",
+    youtube: "https://youtu.be/WW_DdSJ9h7c",
   },
   {
     title: "Sports Simplified",
@@ -25,6 +17,14 @@ const projects = [
     youtube: "https://youtu.be/nJfliL5gyJc",
   },
   {
+    title: "Stock Stop",
+    description: "Simplified listing of stock information with news",
+    image: "stock-stop.png",
+    url: "https://kspaffk.github.io/Project1/",
+    github: "https://github.com/kspaffk/Project1",
+    youtube: "https://youtu.be/vIWwl1OMZkw",
+  },
+  {
     title: "Weather Station",
     description:
       "Simplified weather app for those who want information quickly",
@@ -34,20 +34,20 @@ const projects = [
     youtube: "https://youtu.be/s0WmDIK_47Y",
   },
   {
-    title: "Simplicity Via Clarity",
-    description: "See who is on your website and what they are doing",
-    image: "simplicity-via-clarity.png",
-    url: "https://simplicityviaclarity.com/",
-    github: "https://github.com/ccollins1544/simplicityviaclarity",
-    youtube: "https://youtu.be/WW_DdSJ9h7c",
-  },
-  {
     title: "What Do You Want?",
     description: "Restaurant polling app",
     image: "what-do-you-want.png",
     url: "http://wdyw.space",
     github: "https://github.com/jonvictory/projectUno",
     youtube: "https://youtu.be/8fam_qGb9fE",
+  },
+  {
+    title: "What's in Your Pantry",
+    description: "Find a recipe based on the items in your pantry",
+    image: "whats-in-your-pantry.png",
+    url: "https://collinchiz.github.io/Project-1/",
+    github: "https://github.com/collinchiz/Project-1",
+    youtube: "https://youtu.be/n5rXBLownQs",
   },
 ]
 export default projects
