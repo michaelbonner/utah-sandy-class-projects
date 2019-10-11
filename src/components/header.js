13be054d0bfd5b14c8ed65f10830fbe6b7c04c04
Michael Bonner <michaelwbonner@gmail.com>
2019-10-11 15:01:59 -0600

    update project links

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,12 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Image from "./image"
 
 const Header = ({ siteTitle }) => (
-  <header className="bg-crimson-500 mb-2">
-    <div className="container px-4 py-8 m-auto">
+  <header className="bg-crimson-500 mb-8 shadow-xl">
+    <div className="container px-6 py-8 m-auto">
       <h1>
-        <span className="max-h-32 float-right">
-          <Image
-            filename="block-u-white.png"
-            alt="University of Utah"
-            className="w-8 inline-block"
-          />
-        </span>
-        <Link to="/" className="text-white no-underline text-lg uppercase">
+        <Link to="/" className="text-white no-underline text-2xl uppercase">
           {siteTitle}
         </Link>
       </h1>
---
+++ src/data/projects.js
@@ -0,0 +1,53 @@
+const projects = [
+  {
+    title: "What's in Your Pantry",
+    description: "Find a recipe based on the items in your pantry",
+    image: "whats-in-your-pantry.png",
+    url: "https://collinchiz.github.io/Project-1/",
+    github: "https://github.com/collinchiz/Project-1",
+    youtube: "https://youtu.be/n5rXBLownQs",
+  },
+  {
+    title: "Stock Stop",
+    description: "Simplified listing of stock information with news",
+    image: "stock-stop.png",
+    url: "https://kspaffk.github.io/Project1/",
+    github: "https://github.com/kspaffk/Project1",
+    youtube: "https://youtu.be/vIWwl1OMZkw",
+  },
+  {
+    title: "Sports Simplified",
+    description:
+      "Find out what's happening around the sports team venue. Never get left in the rain uprepared again",
+    image: "sports-simplified.png",
+    url: "https://themtwa.github.io/Project-1/",
+    github: "https://github.com/themtwa/Project-1",
+    youtube: "https://youtu.be/nJfliL5gyJc",
+  },
+  {
+    title: "Weather Station",
+    description:
+      "Simplified weather app for those who want information quickly",
+    image: "weather-station.png",
+    url: "https://nmikstas.github.io/group-project-1b/",
+    github: "https://github.io/nmikstas/group-project-1b",
+    youtube: "https://youtu.be/s0WmDIK_47Y",
+  },
+  {
+    title: "Simplicity Via Clarity",
+    description: "See who is on your website and what they are doing",
+    image: "simplicity-via-clarity.png",
+    url: "https://simplicityviaclarity.com/",
+    github: "https://github.com/ccollins1544/simplicityviaclarity",
+    youtube: "https://youtu.be/WW_DdSJ9h7c",
+  },
+  {
+    title: "What Do You Want?",
+    description: "Restaurant polling app",
+    image: "what-do-you-want.png",
+    url: "http://wdyw.space",
+    github: "https://github.com/jonvictory/projectUno",
+    youtube: "https://youtu.be/8fam_qGb9fE",
+  },
+]
+export default projects
