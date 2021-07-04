6f1ded2ec4c82e044a166e229288da12496692a4
Michael Bonner <michaelwbonner@gmail.com>
2021-07-04 12:18:13 -0600

    change case of seo component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 import ProjectNumber from "../components/project-number"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Class Projects" />
+    <Seo title="Class Projects" />
     <ProjectNumber number="1" />
     <ProjectNumber number="2" />
     <ProjectNumber number="3" />
