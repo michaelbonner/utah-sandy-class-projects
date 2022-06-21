c8b2f0837fc97c87263391bdae912a7481150e08
Michael Bonner <michaelwbonner@gmail.com>
2022-06-20 21:27:44 -0600

    update test config to prevent warnings

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
   },
   moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    ".+\\.(css|style|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
   testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
@@ -11,7 +11,9 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: ``,
   },
-  testURL: `http://localhost`,
-  setupFiles: [`<rootDir>/loadershim.js`],
+  setupFiles: [],
   setupFilesAfterEnv: ["<rootDir>/setup-test-env.js"],
+  testEnvironmentOptions: {
+    url: `http://localhost`,
+  },
 }
